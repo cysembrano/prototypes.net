e2f6fb128c233d60120f5e1079c13804e35cd190
Cy Sembrano <cyrus.sembrano@gmail.com>
2015-06-11 15:41:04 +1200

    Decorator pattern

--- Courses/DesignPatternsLibrary/Decorator/Program.cs
+++ Courses/DesignPatternsLibrary/Decorator/Program.cs
@@ -19,6 +19,79 @@ namespace Decorator
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Decorator Pattern\n");
+
+            IComponent component = new Component();
+            Display("1. Basic Component: " , component);
+            Display("2. A-Decorated Component: ", new DecoratorA(component));
+            Display("3. B-Decorated Component: ", new DecoratorB(component));
+            Display("3. B-A-Decorated Component: ", new DecoratorB(new DecoratorA(component)));
+
+            //Explicit DecoratorB
+            DecoratorB b = new DecoratorB(new Component());
+            Display("5. A-B-Decorated: ", new DecoratorA(b));
+
+            //Invoke added state
+            Console.WriteLine("\t\t\t" + b.addedState + b.AddedBehavior());
+
+        }
+
+        static void Display(string s, IComponent c)
+        {
+            Console.WriteLine(s + c.Operation());
+        }
+    }
+
+    interface IComponent
+    {
+        string Operation();
+    }
+
+    class Component : IComponent
+    {
+
+        public string Operation()
+        {
+            return "I am walking ";
+        }
+    }
+
+    class DecoratorA : IComponent
+    {
+        IComponent component;
+        public DecoratorA(IComponent c)
+        {
+            component = c;
+        }
+
+        public string Operation()
+        {
+            string s = component.Operation();
+            s += "and listening to Classic FM ";
+            return s;
+        }
+    }
+
+    class DecoratorB : IComponent
+    {
+        IComponent component;
+        public string addedState = "past the Coffee Shop ";
+
+        public DecoratorB(IComponent c)
+        {
+            component = c;
+        }
+
+        public string Operation()
+        {
+            string s = component.Operation();
+            s += "to school ";
+            return s;
+        }
+
+        public string AddedBehavior()
+        {
+            return "and I bought a cappuccino ";
         }
     }
 }
