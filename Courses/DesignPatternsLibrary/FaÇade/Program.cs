bd2b4dfe489d55c7cd15739e667e2930e65b0f44
Cy Sembrano <cyrus.sembrano@gmail.com>
2015-06-23 17:18:27 +1200

    Facade Commit

--- Courses/DesignPatternsLibrary/FaÇade/Program.cs
+++ Courses/DesignPatternsLibrary/FaÇade/Program.cs
@@ -18,6 +18,47 @@ namespace FaÇade
     {
         static void Main(string[] args)
         {
+            Facade.Operation1();
+            Facade.Operation2();
         }
     }
+
+    #region Multiple Subsystems
+    internal class SubSystemA
+    {
+        internal string A1() { return "Subsystem A, Method A1\n"; }
+        internal string A2() { return "Subsystem A, Method A2\n"; }
+    }
+
+    internal class SubSystemB
+    {
+        internal string B1() { return "Subsystem B, Method B1\n"; }
+
+    }
+    internal class SubSystemC
+    {
+        internal string C1() { return "Subsystem C, Method C1\n"; }
+        internal string C2() { return "Subsystem C, Method C2\n"; }
+        internal string C3() { return "Subsystem C, Method C3\n"; }
+    }
+    #endregion
+
+    #region FaÇade
+    public static class Facade
+    {
+        static SubSystemA a = new SubSystemA();
+        static SubSystemB b = new SubSystemB();
+        static SubSystemC c = new SubSystemC();
+
+        public static void Operation1()
+        {
+            Console.WriteLine("Operation 1\n" + a.A1() + a.A2() + b.B1());
+        }
+        public static void Operation2()
+        {
+            Console.WriteLine("Operation 2\n" + b.B1() + c.C1());
+        }
+    }
+    #endregion
+
 }
