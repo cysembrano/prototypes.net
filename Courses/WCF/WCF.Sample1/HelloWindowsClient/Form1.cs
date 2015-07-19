736bc1be6e3a54ac3a6928cb94202b5eace30ff5
Cy Sembrano <cyrus.sembrano@gmail.com>
2015-07-20 00:32:34 +1200

    Clients and Hosts

---
+++ Courses/WCF/WCF.Sample1/HelloWindowsClient/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace HelloWindowsClient
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            HelloService.HelloServiceClient client = new HelloService.HelloServiceClient("NetTcpBinding_IHelloService");
+            this.label1.Text = client.GetMessage(this.textBox1.Text);
+        }
+    }
+}
