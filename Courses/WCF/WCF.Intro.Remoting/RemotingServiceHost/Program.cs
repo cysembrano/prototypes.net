88f194e75f212da3bed7c030328064852654e215
Cy Sembrano <cyrus.sembrano@gmail.com>
2015-07-19 19:06:44 +1200

    Remoting Sample

---
+++ Courses/WCF/WCF.Intro.Remoting/RemotingServiceHost/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Tcp;
+
+namespace RemotingServiceHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var remotingService = new HelloRemotingService.HelloRemotingService();
+
+            var channel = new TcpChannel(8080);
+            ChannelServices.RegisterChannel(channel);
+            RemotingConfiguration.RegisterWellKnownServiceType
+                (typeof(HelloRemotingService.HelloRemotingService), "GetMessage", WellKnownObjectMode.Singleton);
+            Console.WriteLine("Remoting service started @ " + DateTime.Now);
+            Console.ReadLine();
+        }
+    }
+}
