da6d8de7999de87f213a54101ad029f5714e30a8
Cy Sembrano <cyrus.sembrano@gmail.com>
2015-10-30 12:56:45 +1300

    How to Custom Http Handler

---
+++ Courses/ASPNET/CustomHttpHandler/CustomHttpHandler/CsvHandler.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Web;
+
+namespace CustomHttpHandler
+{
+    public class CsvHandler : IHttpHandler
+    {
+        /// <summary>
+        /// You will need to configure this handler in the web.config file of your 
+        /// web and register it with IIS before being able to use it. For more information
+        /// see the following link: http://go.microsoft.com/?linkid=8101007
+        /// </summary>
+        #region IHttpHandler Members
+
+        public bool IsReusable
+        {
+            // Return false in case your Managed Handler cannot be reused for another request.
+            // Usually this would be false in case you have some state information preserved per request.
+            get { return false; }
+        }
+
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.ContentType = "application/csv";
+            context.Response.Write("1,2,3,4 \r\n 5,6,7,8");
+        }
+
+        #endregion
+    }
+}
