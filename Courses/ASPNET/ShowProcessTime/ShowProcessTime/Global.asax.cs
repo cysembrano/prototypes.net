2c686537193c8090c885e8e8cadfbb495118c172
Cy Sembrano <cyrus.sembrano@gmail.com>
2015-10-30 10:56:55 +1300

    Show Process time code sample

---
+++ Courses/ASPNET/ShowProcessTime/ShowProcessTime/Global.asax.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+
+namespace ShowProcessTime
+{
+    public class Global : System.Web.HttpApplication
+    {
+
+        public void Application_BeginRequest(object obj, EventArgs e)
+        {
+            Context.Items["startTime"] = DateTime.Now;
+        }
+
+        public void Application_EndRequest(object obj, EventArgs e)
+        {
+            DateTime t = (DateTime)Context.Items["startTime"];
+            TimeSpan delta = DateTime.Now - t;
+            Response.Write(String.Format("<h5>This page took {0} ms to process</h5>", delta.TotalMilliseconds.ToString()));
+        }
+    }
+}
