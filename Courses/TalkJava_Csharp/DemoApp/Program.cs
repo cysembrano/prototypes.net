a54152ff28b7beb4f73b0af9f4bb49c798d91327
Cy Sembrano <cyrus.sembrano@gmail.com>
2016-04-07 08:57:01 +1200

    First Commit

---
+++ Courses/TalkJava_Csharp/DemoApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            java.lang.System.@out.println("Hello from Java in .NET!");
+        }
+    }
+}
