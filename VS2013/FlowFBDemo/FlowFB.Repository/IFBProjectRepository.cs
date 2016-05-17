82ff8a55ba20867ab2b0b1f532de2aa2309692c8
Cy Sembrano <cyrus.sembrano@gmail.com>
2016-05-18 11:22:55 +1200

    Initial Commit

---
+++ VS2013/FlowFBDemo/FlowFB.Repository/IFBProjectRepository.cs
@@ -0,0 +1,16 @@
+﻿using FlowFB.Repository.Filters;
+using FlowFB.Repository.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlowFB.Repository
+{
+    public interface IFBProjectRepository
+    {
+        IEnumerable<FBProject> SearchFBProjects(FBProjectFilter filter);
+
+    }
+}
---
+++ VS2013/FlowFBDemo/FlowFB.Web/App_Start/BundleConfig.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Web;
+using System.Web.Optimization;
+
+namespace FlowFB.Web
+{
+    public class BundleConfig
+    {
+
+        public static void AddDefaultIgnorePatterns(IgnoreList ignoreList)
+        {
+            ignoreList.Clear();
+            if (ignoreList != null)
+            {
+
+                ignoreList.Ignore("*.intellisense.js");
+                ignoreList.Ignore("*-vsdoc.js");
+                ignoreList.Ignore("*.debug.js", OptimizationMode.WhenEnabled);                
+                return;
+            }
+            else
+            {
+                throw new ArgumentNullException("ignoreList");
+            }
+
+        }
+
+
+        // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            AddDefaultIgnorePatterns(bundles.IgnoreList);
+
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
+                        "~/Scripts/jquery-ui-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.unobtrusive*",
+                        "~/Scripts/jquery.validate*"));
+
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
+                        "~/Content/themes/base/jquery.ui.core.css",
+                        "~/Content/themes/base/jquery.ui.resizable.css",
+                        "~/Content/themes/base/jquery.ui.selectable.css",
+                        "~/Content/themes/base/jquery.ui.accordion.css",
+                        "~/Content/themes/base/jquery.ui.autocomplete.css",
+                        "~/Content/themes/base/jquery.ui.button.css",
+                        "~/Content/themes/base/jquery.ui.dialog.css",
+                        "~/Content/themes/base/jquery.ui.slider.css",
+                        "~/Content/themes/base/jquery.ui.tabs.css",
+                        "~/Content/themes/base/jquery.ui.datepicker.css",
+                        "~/Content/themes/base/jquery.ui.progressbar.css",
+                        "~/Content/themes/base/jquery.ui.theme.css"));
+
+
+            bundles.Add(new StyleBundle("~/Templates/css/styles").Include(
+                        "~/Templates/css/bootstrap.min.css",
+                        "~/Templates/css/bootstrap-responsive.min.css",
+                        "~/Templates/css/font-awesome.min.css",
+                        "~/Templates/css/style.css"));
+
+            bundles.Add(new StyleBundle("~/Templates/js/scripts").Include(
+                        "~/Templates/js/bootstrap.js"));
+
+
+
+
+        }
+
+
+
+    }
+}
