6f96c9668de748eca0e5010094b6d9e23ca3d41f
Cy Sembrano <cyrus.sembrano@gmail.com>
2016-06-01 14:44:09 +1200

    Intro to webAPI

---
+++ Courses/VS2013/WebApi/Intro/HelloWebAPIDemoTemplated/Controllers/ValuesController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace HelloWebAPIDemoTemplated.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        static List<string> data = initList();
+
+        private static List<string> initList()
+        {
+            var ret = new List<string>();
+            ret.Add("val1");
+            ret.Add("val2");
+            return ret;
+        }
+
+        // GET api/values
+        public IEnumerable<string> Get()
+        {
+            return data;
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return data[id];
+        }
+
+        // POST api/values
+        public void Post([FromBody]string value)
+        {
+            data.Add(value);
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+            data.RemoveAt(id);
+        }
+    }
+}
