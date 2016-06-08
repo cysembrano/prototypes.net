cd4170ba27641108fca83efb4a6ea363ab64e56e
Cy Sembrano <cyrus.sembrano@gmail.com>
2016-06-08 13:47:39 +1200

    First Finished Version

--- Courses/VS2013/Flow/Canon/FlowHTTPService/Web/Controllers/ActionController.cs
+++ Courses/VS2013/Flow/Canon/FlowHTTPService/Web/Controllers/ActionController.cs
@@ -1,9 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Configuration;
 using System.Net;
 using System.Net.Http;
+using System.Text;
 using System.Web.Http;
+using System.Xml.Linq;
+using System.Xml.XPath;
 using Web.FlowCOM;
 using Web.Logging;
 
@@ -12,86 +14,76 @@ namespace Web.Controllers
     public class ActionController : ApiController
     {
         // GET api/values
-        public HttpResponseMessage Get()
+        public HttpResponseMessage Get(string id, string value)
         {
-            return Request.CreateResponse<string>(HttpStatusCode.OK, @"Usage: ~/Action/{GUID} | Output: GUID logID");
+            return ProcessAction("GET", id, value);
         }
-
-        // GET api/values/5
-        public HttpResponseMessage Get(string id)
-        {
-            if (!String.IsNullOrWhiteSpace(id))
-            {
-                string output;
-                bool result = FlowAPIObject.ExecuteActionSyncWithParams(id,null,out output);
-                if (!result)
-                {
-                    Log4NetManager.Instance.Error(this.GetType(), String.Format("Flow Action {0} was not called.", id));
-                    return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, "Flow Action was not called.");
-                }
-                    
-
-                return Request.CreateResponse<string>(HttpStatusCode.OK, output);
-
-                
-            }                
-            else
-            {
-                return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Action not found");
-            }
-                
-        }
-
-        // GET api/values/5
-        public HttpResponseMessage Get(string id, string input)
-        {
-            if (!String.IsNullOrWhiteSpace(id))
-            {
-                string output;
-                bool result = FlowAPIObject.ExecuteActionSyncWithParams(id, input, out output);
-                if (!result)
-                {
-                    Log4NetManager.Instance.Error(this.GetType(), String.Format("Flow Action {0} was not called.", id));
-                    return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, "Flow Action was not called.");
-                }
-
-
-                return Request.CreateResponse<string>(HttpStatusCode.OK, output);
-
-
-            }
-            else
-            {
-                return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Action not found");
-            }
-
-        }
-
 
         // POST api/values
         public HttpResponseMessage Post(string id, [FromBody]string value)
         {
+            return ProcessAction("POST", id, value);
+        }
 
+        private HttpResponseMessage ProcessAction(string verb, string id, string inputvalue)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("");
+            sb.AppendLine(String.Format(@"{0} VERB BEGINS: {1}\{2} {3}", verb, "Action", id, DateTime.Now));
             if (!String.IsNullOrWhiteSpace(id))
             {
-                string output;
-                bool result = FlowAPIObject.ExecuteActionSyncWithParams(id, value, out output);
+
+
+                string output, logFid;
+                bool result = FlowAPIObject.ExecuteSOAPRequestIn(id, inputvalue, out output, out logFid);
+                sb.AppendLine(String.Format(@"{0} VERB INPUT: {1}",verb, inputvalue));
+
+                string modoutput = GarnishResponseValue(output);
+                sb.AppendLine(String.Format(@"{0} VERB OUTPUT: {1}",verb, modoutput));
+
+
                 if (!result)
                 {
-                    Log4NetManager.Instance.Error(this.GetType(), String.Format("Flow Action {0} was not called.", id));
+                    Log4NetManager.Instance.Error(this.GetType(), String.Format("Flow Action {1} was not called.", id));
+                    sb.AppendLine(String.Format(@"{0} VERB ENDS [ERROR-FlowActionCall]: {1}\{2} {3}",verb, "Action", id, DateTime.Now));
+                    Log4NetManager.Instance.Error(this.GetType(), sb);
                     return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, "Flow Action was not called.");
+
                 }
+                sb.AppendLine(String.Format(@"{0} VERB ENDS [SUCCESS]: {1}\{2} {3}", verb, "Action", id, DateTime.Now));
+                Log4NetManager.Instance.Info(this.GetType(), sb);
+                return Request.CreateResponse<string>(HttpStatusCode.OK, modoutput);
 
-
-                return Request.CreateResponse<string>(HttpStatusCode.OK, output);
 
 
             }
             else
             {
+                sb.AppendLine(String.Format(@"{0} VERB ENDS [ERROR-NotFound]: {1}\{2} {3}", "Action", id, DateTime.Now));
+                Log4NetManager.Instance.Error(this.GetType(), sb);
                 return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Action not found");
+
             }
 
+        }
+
+        /// <summary>
+        /// Removes the 
+        /// </summary>
+        private string GarnishResponseValue(string xmlResponse)
+        {
+            bool doGarnish = false;
+            bool tryParseResult = Boolean.TryParse(ConfigurationManager.AppSettings["GarnishXmlEnabled"], out doGarnish);
+            if(tryParseResult && doGarnish)
+            {
+                var document = XDocument.Parse(xmlResponse);
+                return document.XPathSelectElement(ConfigurationManager.AppSettings["GarnishXmlXPath"]).Value;
+            }
+            else
+            {
+                return xmlResponse;
+            }
+            
         }
 
     }
