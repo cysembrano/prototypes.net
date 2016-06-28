86d6878fbfc97e3961a11a51637e2bead5333f25
Cy Sembrano <cyrus.sembrano@gmail.com>
2016-06-28 14:29:58 +1200

    Initial Commit

---
+++ VS2010/FlowAPI/FlowAPI.Objects/FlowConfigLogin.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FlowAppInterface.Objects
+{
+    public class FlowConfigLogin
+    {
+        private readonly string _serverName;
+        private readonly string _databaseName;
+        private readonly string _userName;
+        private readonly string _passWord;
+
+        public string ServerName
+        {
+            get { return _serverName; }
+        }
+
+        public string DatabaseName
+        {
+            get { return _databaseName; }
+        }
+
+        public string Username
+        {
+            get { return _userName; }
+        }
+
+        public string Password
+        {
+            get { return _passWord; }
+        }
+
+        public FlowConfigLogin(string servername, string databasename, string username, string password)
+        {
+            _serverName = servername;
+            _databaseName = databasename;
+            _userName = username;
+            _passWord = password;
+        }
+
+    }
+}
