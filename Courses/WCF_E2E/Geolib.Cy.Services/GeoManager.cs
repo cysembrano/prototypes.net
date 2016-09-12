bba7bb0ea1cbd40e844ce0359bf790cab8c46987
Cy Sembrano <cyrus.sembrano@gmail.com>
2016-09-12 14:44:59 +1200

    GeoManager implements IGeoService

---
+++ Courses/WCF_E2E/Geolib.Cy.Services/GeoManager.cs
@@ -0,0 +1,32 @@
+﻿using Geolib.Cy.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geolib.Cy.Services
+{
+    public class GeoManager : IGeoService
+    {
+        public ZipCodeData GetZipInfo(string zip)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<string> GetStates(bool primaryOnly)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<ZipCodeData> GetZips(string state)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<ZipCodeData> GetZips(string zip, int range)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
