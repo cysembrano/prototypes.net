08ebea35fe1f689084ae05430d36d6c73397fd21
Cy Sembrano <cyrus.sembrano@gmail.com>
2017-02-09 15:48:20 +1300

    Created service and host

---
+++ Courses/WCF/WCF.P15.ExceptionHandling/CalculatorService/CalculatorService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CalculatorService
+{
+    //[ServiceBehavior(IncludeExceptionDetailInFaults=true)] //Another option to include fault exception details.
+    public class CalculatorService : ICalculatorService
+    {
+        public int Divide(int Numerator, int Denominator)
+        {
+            return Numerator / Denominator;
+        }
+    }
+}
